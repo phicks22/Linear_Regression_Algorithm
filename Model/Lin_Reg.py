5b3869bfaa5147155c5ff8fc9a9b8604213290af
phicks22 <65268541+phicks22@users.noreply.github.com>
2021-04-09 10:59:12 -0500

    Update Lin_Reg.py

--- Model/Lin_Reg.py
+++ Model/Lin_Reg.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 class Linreg:
 
+    def __init__(self, file):
+        self.file = file
+        self.error_dict = dict()
+        self.err_mult_dict = dict()
+        self.slope = slope
+        self.y_int = y_int
+
+
     def open_file(self, fileAsString):
         data = pd.read_csv(fileAsString)
 
@@ -16,14 +24,27 @@ class Linreg:
         return error_dict
 
     def error_mult(self, data):
-        error_mult_dict = []
+        error_mult_dict = dict()
         for row in col1 of dataframe:
             error_mult_dict = row - np.mean(data)
 
         return error_mult_dict
 
-    def slope(self, data):
+    def line_equation(self, error_mult_dict, error_dict):
+        sum_squares = 0
+        for i in error_mult_dict[0]:
+            sum_squares += i
 
+        x_mean_diff = 0
+        for i in error_dict[0]:
+            x_mean_diff += i
+
+        self.slope = sum_squares / x_mean_diff
+
+        return self.slope
+
+    def y_int(self):
+        intercept = 
 
 
 
