2c1f89af43a1fa4d30fdc2e6f4b30ab788c6f233
phicks22 <65268541+phicks22@users.noreply.github.com>
2021-04-09 13:59:04 -0500

    updated read in function

--- Data/Language_GreyMatter_Data.py
+++ Data/Language_GreyMatter_Data.py
@@ -1,5 +1,13 @@
-import pandas as pd
+import csv
 
-language_data = pd.read_csv("/Users/parkerhicks/Desktop/Biostats_data/chap16q15LanguageGreyMatter.csv")
+dict_list = list()
+with open("/Users/parkerhicks/Desktop/Biostats_data/chap16q15LanguageGreyMatter.csv") as f:
+    records = csv.DictReader(f)
+    for row in records:
+        dict_list.append(row)
+    print(dict_list)
 
-print(language_data)
+example = dict_list[0]
+print(example)
+
+
